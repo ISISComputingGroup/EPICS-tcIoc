d2e09c760b8d84c7d19feed11f489d8b8186ec00
Dominic Oram <dominic.oram@stfc.ac.uk>
2019-09-02 11:15:41 +0100

    Reverted modified file

--- iocBoot/ioctcIoc/st-common.cmd
+++ iocBoot/ioctcIoc/st-common.cmd
@@ -1,9 +1,6 @@
 
-tcSetAlias("PLC:TEST:")
 tcSetScanRate(10, 5)
-#tcLoadRecords ("$(TPY_FILE)", "-ps -devtc -rd -cp -nd")
-#tcGenerateList("$(TOP)\test_PLC\TestPLC\TestPLC\TestCode\TestCode.tpy.txt","-l -rn -yi -cp")
-tcLoadRecords ("C:\Jenkins\workspace\line_Ticket3991_run_system_tests\dummy_PLC\TestPLC\TestCode\TestCode.tpy", "-eo -devtc")
+tcLoadRecords ("Plc.tpy", "-eo -devtc")
 #tcLoadRecords ("DrivePlc.tpy", "-ea -devtc")
 #tcLoadRecords ("DrivePlc.tpy", "-eo -devtc")
 
