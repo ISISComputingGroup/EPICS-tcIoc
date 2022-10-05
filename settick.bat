68b30a2f7238afb25025281996dfa9201e4be7e8
Jack Harper <jack.harper@stfc.ac.uk>
2022-10-05 11:52:03 +0100

    move settick scripts to beckhofftestrunner

--- settick.bat
+++
@@ -1,4 +0,0 @@
-echo this should be run as admin and requires a reboot afterwards.
-echo note this may cause all IOCs to error with a "time discontinuity" - to resolve this you will need to run unsettick.bat
-bcdedit /set disabledynamictick yes
-bcdedit /set useplatformtick yes
