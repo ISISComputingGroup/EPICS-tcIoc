3dcd44cc8cf37e929b35846ea4a31837ca224043
Jack Harper <jack.harper@stfc.ac.uk>
2022-12-02 11:45:37 +0000

    fixing limits

--- motorSupportApp/src/devMotorAxis.cpp
+++ motorSupportApp/src/devMotorAxis.cpp
@@ -432,17 +432,18 @@ asynStatus devMotorAxis::poll(bool *moving) {
   
   int dir;
   pC_->getIntegerParam(pC_->motorRecDirection_, &dir);
-  if (dir == 1){
+  if (dir == 0){
     // Move is positive from mtr rec
     setIntegerParam(pC_->motorStatusDirection_, st_axis_status.bDirection);
-    setIntegerParam(pC_->motorStatusLowLimit_, !st_axis_status.bLimitBwd);
-	  setIntegerParam(pC_->motorStatusHighLimit_, !st_axis_status.bLimitFwd);
   } else {
     // Move is negative from mtr rec, so flip direction and limits
     setIntegerParam(pC_->motorStatusDirection_, !st_axis_status.bDirection);
-    setIntegerParam(pC_->motorStatusLowLimit_, !st_axis_status.bLimitFwd);
-	  setIntegerParam(pC_->motorStatusHighLimit_, !st_axis_status.bLimitBwd);
   }
+  // these are flipped by the motor record by the above
+  setIntegerParam(pC_->motorStatusLowLimit_, !st_axis_status.bLimitBwd);
+  setIntegerParam(pC_->motorStatusHighLimit_, !st_axis_status.bLimitFwd);
+  setIntegerParam(pC_->motorHighLimit_, !st_axis_status.bLimitFwd);
+  setIntegerParam(pC_->motorLowLimit_, !st_axis_status.bLimitBwd);
 
 	
 	// Get the actual position
